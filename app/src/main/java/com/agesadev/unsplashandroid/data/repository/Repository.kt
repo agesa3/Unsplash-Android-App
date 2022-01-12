1610c9f4031df37232b1b096743878d6ac78568e
agesa3 <collinsagesa2000@gmail.com>
2022-01-12 10:36:06 +0300

    implimeneted the repository class

--- app/src/main/java/com/agesadev/unsplashandroid/data/repository/Repository.kt
+++ app/src/main/java/com/agesadev/unsplashandroid/data/repository/Repository.kt
@@ -1,4 +1,31 @@
 package com.agesadev.unsplashandroid.data.repository
 
-class Repository {
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.agesadev.unsplashandroid.data.local.UnsplashDatabase
+import com.agesadev.unsplashandroid.data.paging.UnsplashRemoteMediator
+import com.agesadev.unsplashandroid.data.remote.UnsplashApi
+import com.agesadev.unsplashandroid.models.UnsplashImage
+import com.agesadev.unsplashandroid.utils.Constants.ITEMS_PER_PAGE
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class Repository @Inject constructor(
+    private val unsplashApi: UnsplashApi,
+    private val unsplashDatabase: UnsplashDatabase
+) {
+    @ExperimentalPagingApi
+    fun getAllImages(): Flow<PagingData<UnsplashImage>> {
+        val pagingSourceFactory = { unsplashDatabase.unsplashImageDao().getAllImages() }
+        return Pager(
+            config = PagingConfig(pageSize = ITEMS_PER_PAGE),
+            remoteMediator = UnsplashRemoteMediator(
+                unsplashApi = unsplashApi,
+                unsplashDatabase = unsplashDatabase
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+    }
 }
